b4f27db5b9d0797cc5e93f3eae266a83f1f9d819
Minh <yamchahuyvs@gmail.com>
2023-04-09 20:14:35 +0700

    [not yet]

--- Assets/Script/PlayerLives.cs
+++ Assets/Script/PlayerLives.cs
@@ -19,25 +19,30 @@ public class PlayerLives : MonoBehaviour
     public CinemachineCollisionImpulseSource cinemachineImpulseSource;
     [SerializeField]
     private CinemachineVirtualCamera cinemachine;
-    
+    private float time;
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         boxCollider= GetComponent<BoxCollider2D>();
     }
-    private void OnCollisionEnter2D(Collision2D coll)
+    
+    private void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.CompareTag("Monster"))
+        if (coll.gameObject.CompareTag("Monster")&& !animator.GetBool("Dead"))
         {
+            if (Time.time < time) return;
             lives--;
+            time = Time.time+0.3f;
+            Debug.Log("mon " + lives);
             if (lives == 0)
             {
                 StartCoroutine(Die());
                 return;
             }
+            Debug.Log("no");
             animator.SetTrigger("Hurt");
-            Rigidbody2D enemyRigidbody = coll.collider.GetComponent<Rigidbody2D>(); // Lấy Rigidbody của đối tượng kẻ địch
+            Rigidbody2D enemyRigidbody = coll.GetComponent<Rigidbody2D>(); // Lấy Rigidbody của đối tượng kẻ địch
             cinemachineImpulseSource.GenerateImpulse(Camera.main.transform.forward);
             if (enemyRigidbody != null) // Kiểm tra nếu đối tượng kẻ địch có Rigidbody
             {
@@ -47,20 +52,23 @@ public class PlayerLives : MonoBehaviour
             }
             Invoke("rebind", 0.3f);
         }
-        else if ((coll.gameObject.CompareTag("Bullet")))
+        else if ((coll.CompareTag("Bullet"))&& animator.GetBool("Dead"))
         {
             lives--;
+            
             if (lives == 0)
             {
                 StartCoroutine(Die());
                 return;
             }
+            
             cinemachineImpulseSource.GenerateImpulse(Camera.main.transform.forward);
             animator.SetTrigger("Hurt");
             Invoke("rebind", 0.3f);
         }
         
     }
+    
     private void rebind()
     {
         animator.Rebind();
@@ -74,7 +82,8 @@ public class PlayerLives : MonoBehaviour
         boxCollider.isTrigger = true;
         yield return new WaitForSeconds(1f);
         Time.timeScale = 1f;
-        Destroy(this, 2f);
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
     }
    
 }
